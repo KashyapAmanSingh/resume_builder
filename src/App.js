0171c4b2c29ab49f9e0efbb4e4e45e11e14391e7
KashyapAmanSingh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-03-16 17:22:53 +0530

    app.js with responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import { BrowserRouter, Form, Route, Routes } from "react-router-dom";
 
-import About from "./component/About";
-import Choosetemplate from "./component/ChooseTemplate";
-import Preview from "./component/Preview";
-import FillDetails from "./component/FillDetails"
-import Download from "./component/Download";
-import Home from "./component/Home";
-import Navbar from "./component/Navbar";
+import './App.css';
+import React from 'react'  
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Home from './component/Home';
+import Download from './component/Download';
+import Choosetemplate from './component/ChooseTemplate';
+import Preview from './component/Preview';
+import Navb from './component/Navbar';
+import About from './component/About';
+import { GetResume } from './component/GetResume';
+import  Details from './component/Details';
+import { Redirect, Route,Switch } from 'react-router-dom';
+// import PillNavb from './component/SideNavb';
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 function App() {
   return (
-    <>
-    
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
 
-     <Route path="/"  element={<Home/>} ></Route>                                                  {/* Mainly show what is in about Component */}
-     <Route path="/About"  element={<About/>} ></Route> 
-     <Route path="/Choosetemplate" element = {<Choosetemplate/>} > </Route>
-     <Route path="/Preview" element = {<Preview/>} > </Route>
-     <Route path="/FillDetails" element = {<FillDetails />} > </Route>
-     <Route path="/Download" element = {<Download/>} > </Route>
-     <Route path="*"  element={<h1> PAGE not found 404 </h1>}  />   
-     </Routes>
-       </BrowserRouter>
-</>
-  );
-}
 
+<>  
+
+
+    <Navb/>
+    {/* <PillNavb/> */}
+<Switch>
+<Route exact path='/' component={Home}/>
+  <Route exact path="/About"  component={About} ></Route> 
+  <Route exact  path='/GetResume' component={GetResume} />
+ <Route  exact path="/Choosetemplate" component = {Choosetemplate} > </Route>
+ <Route exact  path='/Details' component={Details} />
+  <Route exact  path='/preview' component={Preview} />
+  <Route exact  path='/Download' component={Download} />
+
+ 
+       <Redirect to="/ " component={Home}/>
+         </Switch>
+         
+        
+         
+          </>
+      );
+     }
+     
+   
+      
+      
+ 
 export default App;
