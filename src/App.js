67e98f1cbd4d2b4777419025348eef836e428e96
KashyapAmanSingh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-03-20 21:50:35 +0530

    for krushna try once now everything is working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React from 'react'  
+import React from 'react'
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Home from './component/Home';
 import Download from './component/Download';
@@ -9,40 +9,47 @@ import Preview from './component/Preview';
 import Navb from './component/Navbar';
 import About from './component/About';
 import { GetResume } from './component/GetResume';
-import  Details from './component/Details';
-import { Redirect, Route,Switch } from 'react-router-dom';
-// import PillNavb from './component/SideNavb';
+import Details from './component/Details';
+import Form from './Form';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Alancontainer from './component/Alancontainer';
 
 function App() {
-  return (
 
 
-<>  
 
 
-    <Navb/>
-    {/* <PillNavb/> */}
-<Switch>
-<Route exact path='/' component={Home}/>
-  <Route exact path="/About"  component={About} ></Route> 
-  <Route exact  path='/GetResume' component={GetResume} />
- <Route  exact path="/Choosetemplate" component = {Choosetemplate} > </Route>
- <Route exact  path='/Details' component={Details} />
-  <Route exact  path='/preview' component={Preview} />
-  <Route exact  path='/Download' component={Download} />
 
- 
-       <Redirect to="/ " component={Home}/>
-         </Switch>
-         
-        
-         
+     return (
+
+
+          <>
+
+               <Navb />
+
+               <Alancontainer />
+               <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path="/About" component={About} />
+                    <Route exact path='/GetResume' component={GetResume} />
+                    <Route exact path="/Choosetemplate" component={Choosetemplate} /> 
+                    <Route exact path='/Details' component={Details} />
+                    <Route exact path='/preview' component={Preview} />
+                    <Route exact path='/Download' component={Download} />
+                    <Route exact path='/form' component={Form} />
+
+                    <Redirect to="/ " component={Home} />
+
+               </Switch>
+
+
+
           </>
-      );
-     }
-     
-   
-      
-      
- 
+     );
+}
+
+
+
+
+
 export default App;
